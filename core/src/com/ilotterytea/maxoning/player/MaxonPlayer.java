0018a53e08d01ea5af1f147ff0f426b2df8248dd
ilotterytea <ilotterytea@proton.me>
2022-08-31 03:17:27 +0600

    класс игрока

---
+++ core/src/com/ilotterytea/maxoning/player/MaxonPlayer.java
@@ -0,0 +1,26 @@
+package com.ilotterytea.maxoning.player;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class MaxonPlayer implements Serializable {
+    public float points;
+    public float multiplier;
+    public ArrayList<MaxonItem> purchasedItems;
+
+    public MaxonPlayer() {
+        this.points = 0;
+        this.multiplier = 1.2f;
+        this.purchasedItems = new ArrayList<>();
+    }
+
+    public void load(MaxonPlayer player) {
+        if (player != null) {
+            this.points = player.points;
+            this.multiplier = player.multiplier;
+
+            this.purchasedItems.clear();
+            this.purchasedItems.addAll(player.purchasedItems);
+        }
+    }
+}
