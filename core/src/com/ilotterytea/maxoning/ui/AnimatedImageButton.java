0867ea1b95756441100037ee3c7cc42e122c214b
ilotterytea <me@ilotterytea.kz>
2022-08-31 03:00:35 +0600

    ImageButton for AnimatedImage

---
+++ core/src/com/ilotterytea/maxoning/ui/AnimatedImageButton.java
@@ -0,0 +1,24 @@
+package com.ilotterytea.maxoning.ui;
+
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
+
+public class AnimatedImageButton extends ImageButton {
+    public AnimatedImageButton(AnimatedImage image) {
+        super(image.getDrawable());
+        ImageButtonStyle style = new ImageButtonStyle();
+
+        style.up = image.getDrawable();
+        super.setStyle(style);
+    }
+
+    public void setDrawable(Drawable drawable) {
+        ImageButtonStyle style = new ImageButtonStyle();
+
+        style.up = drawable;
+        super.setStyle(style);
+    }
+
+    @Override public void act(float delta) {
+    }
+}
