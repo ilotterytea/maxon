21ccf305d6468846ef99b79a60c0a1eb83eb7d93
NotDankEnough <hatesociety@proton.me>
2022-09-01 14:57:59 +0600

    id for maxon item

--- core/src/com/ilotterytea/maxoning/player/MaxonItem.java
+++ core/src/com/ilotterytea/maxoning/player/MaxonItem.java
@@ -3,17 +3,21 @@ package com.ilotterytea.maxoning.player;
 import com.ilotterytea.maxoning.ui.AnimatedImage;
 
 public class MaxonItem {
+    public int id;
     public String name;
     public String desc;
     public AnimatedImage icon;
     public MaxonItemEnum type;
     public float price;
+    public float multiplier;
 
-    public MaxonItem(String name, String desc, AnimatedImage icon, MaxonItemEnum type, float price) {
+    public MaxonItem(int id, String name, String desc, AnimatedImage icon, MaxonItemEnum type, float price, float multiplier) {
+        this.id = id;
         this.name = name;
         this.desc = desc;
         this.icon = icon;
         this.type = type;
         this.price = price;
+        this.multiplier = multiplier;
     }
 }
