bd899335e58f5ae30e883c4d3219d03687cec68f
ilotterytea <me@ilotterytea.kz>
2022-09-01 19:55:09 +0600

    🚗 1.0.0 -> 1.0.1

--- core/src/com/ilotterytea/maxoning/MaxonConstants.java
+++ core/src/com/ilotterytea/maxoning/MaxonConstants.java
@@ -2,6 +2,6 @@ package com.ilotterytea.maxoning;
 
 public class MaxonConstants {
     public static final String GAME_NAME = "Maxon Petting Simulator";
-    public static final String GAME_VERSION = "Alpha 1.0.0";
+    public static final String GAME_VERSION = "Alpha 1.0.1";
     public static final String GAME_PUBLISHER = "iLotterytea";
 }
