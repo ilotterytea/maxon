3be466241901ca09912015a79c3efc6b738e9368
ilotterytea <ilotterytea@proton.me>
2022-09-01 20:06:34 +0600

    create the directories if they don't exist

--- core/src/com/ilotterytea/maxoning/utils/serialization/GameDataSystem.java
+++ core/src/com/ilotterytea/maxoning/utils/serialization/GameDataSystem.java
@@ -1,15 +1,20 @@
 package com.ilotterytea.maxoning.utils.serialization;
 
 import com.ilotterytea.maxoning.player.MaxonPlayer;
+import com.ilotterytea.maxoning.utils.OsUtils;
 
 import java.io.*;
 
 public class GameDataSystem {
-    private static final File file = new File(System.getProperty("user.home") + "/MaxoningSavegame.sav");
+    private static final File dir = new File(OsUtils.getUserDataDirectory(".maxoning"));
+    private static final File file = new File(dir.getPath() + "/savegame.sav");
 
     public static boolean exists() { return file.exists(); }
 
     public static void SaveData(MaxonPlayer player) throws IOException {
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
         FileOutputStream fo = new FileOutputStream(file);
         ObjectOutputStream out = new ObjectOutputStream(fo);
 
