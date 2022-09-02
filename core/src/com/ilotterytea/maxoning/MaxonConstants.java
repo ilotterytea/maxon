ee43ea519b630933193e49261b9e98aa2baa0d1a
ilotterytea <me@ilotterytea.kz>
2022-09-02 22:17:09 +0600

    Game data paths

--- core/src/com/ilotterytea/maxoning/MaxonConstants.java
+++ core/src/com/ilotterytea/maxoning/MaxonConstants.java
@@ -1,7 +1,13 @@
 package com.ilotterytea.maxoning;
 
+import com.ilotterytea.maxoning.utils.OsUtils;
+
 public class MaxonConstants {
     public static final String GAME_NAME = "Maxon Petting Simulator";
     public static final String GAME_VERSION = "Alpha 1.0.1";
     public static final String GAME_PUBLISHER = "iLotterytea";
+
+    public static final String GAME_MAIN_FOLDER = OsUtils.getUserDataDirectory(".Maxoning");
+    public static final String GAME_SCREENSHOT_FOLDER = GAME_MAIN_FOLDER + "/screenshots";
+    public static final String GAME_SAVEGAME_FOLDER = GAME_MAIN_FOLDER + "/savegames";
 }
