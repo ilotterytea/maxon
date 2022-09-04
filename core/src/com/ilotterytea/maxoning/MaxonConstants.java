230850c2805426f90eeacc4e1f7aeb3e8ba25a4d
ilotterytea <me@ilotterytea.kz>
2022-09-05 00:34:36 +0600

    я в рот ебал создателей этого фреймворка

--- core/src/com/ilotterytea/maxoning/MaxonConstants.java
+++ core/src/com/ilotterytea/maxoning/MaxonConstants.java
@@ -1,5 +1,7 @@
 package com.ilotterytea.maxoning;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
 import com.ilotterytea.maxoning.utils.OsUtils;
 
 public class MaxonConstants {
@@ -11,4 +13,7 @@ public class MaxonConstants {
     public static final String GAME_MAIN_FOLDER = OsUtils.getUserDataDirectory(".Maxoning");
     public static final String GAME_SCREENSHOT_FOLDER = GAME_MAIN_FOLDER + "/screenshots";
     public static final String GAME_SAVEGAME_FOLDER = GAME_MAIN_FOLDER + "/savegames";
+
+    public static final FileHandle FILE_EN_US = Gdx.files.internal("i18n/en_us.json");
+    public static final FileHandle FILE_RU_RU = Gdx.files.internal("i18n/ru_ru.json");
 }
