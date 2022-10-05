8dccc6e084ec70ba9119c65d0dd3f0ac6bf146f5
ilotterytea <me@ilotterytea.kz>
2022-10-06 01:23:16 +0600

    new debug look letsgo

--- core/src/com/ilotterytea/maxoning/ui/DebugLabel.java
+++ core/src/com/ilotterytea/maxoning/ui/DebugLabel.java
@@ -1,18 +1,19 @@
 package com.ilotterytea.maxoning.ui;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.ilotterytea.maxoning.MaxonConstants;
 
 public class DebugLabel extends Label {
-    private static final String str_placeholder = "%s\n%s fps";
+    private static final String str_placeholder = "%s fps";
 
     public DebugLabel(Skin skin) {
-        super(String.format(str_placeholder, MaxonConstants.GAME_VERSION, Gdx.graphics.getFramesPerSecond()), skin);
+        super(String.format(str_placeholder, Gdx.graphics.getFramesPerSecond()), skin, "debug");
+        super.setColor(Color.LIME);
     }
 
     @Override public void act(float delta) {
-        super.setText(String.format(str_placeholder, MaxonConstants.GAME_VERSION, Gdx.graphics.getFramesPerSecond()));
+        super.setText(String.format(str_placeholder, Gdx.graphics.getFramesPerSecond()));
     }
 }
