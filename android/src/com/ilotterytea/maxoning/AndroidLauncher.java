1a2d2c7b5f0b459b5bfadd8477387467db287559
NotDankEnough <hatesociety@proton.me>
2022-10-21 00:02:31 +0600

    request for permission to read external storage

--- android/src/com/ilotterytea/maxoning/AndroidLauncher.java
+++ android/src/com/ilotterytea/maxoning/AndroidLauncher.java
@@ -1,5 +1,8 @@
 package com.ilotterytea.maxoning;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
@@ -11,6 +14,14 @@ public class AndroidLauncher extends AndroidApplication {
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+
+		// Granting the permissions:
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+			if (this.checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+				this.requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 100);
+			}
+		}
+
 		initialize(new MaxonGame(), config);
 	}
 }
