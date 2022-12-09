a7bdff37f47f766b5976e02c68a092e65a1c4ae4
ilotterytea <me@ilotterytea.kz>
2022-12-10 04:46:13 +0600

    Adjusted to the new design terms.

--- core/src/com/ilotterytea/maxoning/ui/SavegameInfo.kt
+++ core/src/com/ilotterytea/maxoning/ui/SavegameInfo.kt
@@ -26,7 +26,7 @@ class SavegameInfo(
         this.height = 324f
         this.align(Align.top)
 
-        val title = Label(if (sav != null) "Save ${savId + 1} (${sav.name})" else "New Game", skin, "with_background")
+        val title = Label(if (sav != null) "Save ${savId + 1} (${sav.name})" else "New Game", skin, "header_with_bg")
         this.add(title).width(506f).pad(6f).row()
 
         val content = Table()
@@ -43,7 +43,7 @@ class SavegameInfo(
             val pointsLabel = Label("Points", skin)
             content.add(pointsLabel).width(246f).pad(4f)
             // Label for points count:
-            val pointsCLabel = Label(NumberFormatter.format(sav.points.toLong()), skin)
+            val pointsCLabel = Label(NumberFormatter.format(sav.points.toLong()), skin, "value")
             pointsCLabel.setAlignment(Align.right)
             content.add(pointsCLabel).width(246f).pad(4f).row()
 
@@ -52,7 +52,7 @@ class SavegameInfo(
             val mpLabel = Label("Multiplier", skin)
             content.add(mpLabel).width(246f).pad(4f)
             // Label for multiplier count:
-            val mpCLabel = Label("+${NumberFormatter.format(sav.multiplier.toLong())}/click", skin)
+            val mpCLabel = Label("+${NumberFormatter.format(sav.multiplier.toLong())}/click", skin, "value")
             mpCLabel.setAlignment(Align.right)
             content.add(mpCLabel).width(246f).pad(4f).row()
 
@@ -61,7 +61,7 @@ class SavegameInfo(
             val piLabel = Label("Inventory size", skin)
             content.add(piLabel).width(246f).pad(4f)
             // Label for purchased items count:
-            val piCLabel = Label(sav.inv.size.toString(), skin)
+            val piCLabel = Label(sav.inv.size.toString(), skin, "value")
             piCLabel.setAlignment(Align.right)
             content.add(piCLabel).width(246f).pad(4f).row()
 
