ee625d180669974eff356f5ee30aec3dc20cc318
ilotterytea <ilotterytea@proton.me>
2023-06-06 01:16:36 +0600

    add: init sprite batch and shape renderer

--- core/src/main/java/kz/ilotterytea/maxoning/MaxoningGame.java
+++ core/src/main/java/kz/ilotterytea/maxoning/MaxoningGame.java
@@ -1,10 +1,22 @@
 package kz.ilotterytea.maxoning;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class MaxoningGame extends Game {
+    private SpriteBatch batch;
+    private ShapeRenderer shapeRenderer;
     private static MaxoningGame instance;
+
+    public SpriteBatch getBatch() {
+        return batch;
+    }
+
+    public ShapeRenderer getShapeRenderer() {
+        return shapeRenderer;
+    }
 
     public static MaxoningGame getInstance() {
         return instance;
@@ -16,6 +28,8 @@ public class MaxoningGame extends Game {
 
     @Override
     public void create() {
+        batch = new SpriteBatch();
+        shapeRenderer = new ShapeRenderer();
         setScreen(new FirstScreen());
     }
 }
