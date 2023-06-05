2a010846f6f81fe10b990307a7a44d2a9c8d597c
ilotterytea <ilotterytea@proton.me>
2023-06-06 01:14:57 +0600

    add: get instance

--- core/src/main/java/kz/ilotterytea/maxoning/MaxoningGame.java
+++ core/src/main/java/kz/ilotterytea/maxoning/MaxoningGame.java
@@ -4,6 +4,16 @@ import com.badlogic.gdx.Game;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class MaxoningGame extends Game {
+    private static MaxoningGame instance;
+
+    public static MaxoningGame getInstance() {
+        return instance;
+    }
+
+    public MaxoningGame() {
+        instance = this;
+    }
+
     @Override
     public void create() {
         setScreen(new FirstScreen());
