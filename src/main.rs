4b3937b279c431984ce86073793fc5e333512e35
ilotterytea <ilotterytea@proton.me>
2023-06-22 17:27:29 +0600

    add: bevy example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use bevy::{
+    diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin},
+    prelude::*,
+    window::{Window, WindowPlugin},
+    DefaultPlugins,
+};
+use bevy_framepace::FramepacePlugin;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "Maxon Petting Simulator".into(),
+                resolution: (800., 600.).into(),
+                present_mode: bevy::window::PresentMode::AutoVsync,
+                fit_canvas_to_parent: true,
+                prevent_default_event_handling: false,
+                ..default()
+            }),
+            ..default()
+        }))
+        .add_plugin(FrameTimeDiagnosticsPlugin)
+        .add_plugin(LogDiagnosticsPlugin::default())
+        .add_plugin(FramepacePlugin)
+        .run();
 }
