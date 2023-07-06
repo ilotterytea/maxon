da650c8b8e09fba5c48b35c3e280c3dd5b0ee4b8
ilotterytea <ilotterytea@proton.me>
2023-07-07 04:52:35 +0600

    add: assets 2

--- src/assets.rs
+++ src/assets.rs
@@ -1,15 +1,35 @@
-use bevy::{prelude::*, sprite::TextureAtlas};
+use bevy::prelude::*;
 use bevy_asset_loader::asset_collection::AssetCollection;
 
 #[derive(AssetCollection, Resource)]
 pub struct AppAssets {
+    // - - -  L O G O  - - -
     #[asset(path = "sprites/logo.png")]
     pub brand_logo: Handle<Image>,
 
-    #[asset(texture_atlas(tile_size_x = 28., tile_size_y = 28., columns = 7, rows = 7))]
-    #[asset(path = "sprites/widgets.png")]
-    pub menu_widgets: Handle<TextureAtlas>,
+    // - - -  U I  A S S E T S  - - -
+    #[asset(path = "sprites/ui/music_on.png")]
+    pub music_on_widget: Handle<Image>,
 
+    #[asset(path = "sprites/ui/music_off.png")]
+    pub music_off_widget: Handle<Image>,
+
+    #[asset(path = "sprites/ui/quit.png")]
+    pub quit_widget: Handle<Image>,
+
+    #[asset(path = "sprites/ui/fullscreen.png")]
+    pub fullscreen_widget: Handle<Image>,
+
+    #[asset(path = "sprites/ui/windowed.png")]
+    pub windowed_widget: Handle<Image>,
+
+    #[asset(path = "sprites/ui/russian_flag.png")]
+    pub russian_flag_widget: Handle<Image>,
+
+    #[asset(path = "sprites/ui/usa_flag.png")]
+    pub usa_flag_widget: Handle<Image>,
+
+    // - - -  F O N T S  - - -
     #[asset(path = "fonts/main.ttf")]
     pub main_font: Handle<Font>,
 
