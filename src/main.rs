2e4e37acb3f60e55ac93210511d3d30571fc0c83
ilotterytea <ilotterytea@proton.me>
2023-10-08 20:55:10 +0600

    feat: bevy example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use bevy::{
+    diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin},
+    prelude::*,
+    window::PresentMode,
+};
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "Maxon Petting Simulator".into(),
+                present_mode: PresentMode::AutoVsync,
+                ..default()
+            }),
+            ..default()
+        }))
+        // App states
+        .add_state::<AppState>()
+        // Diagnostics
+        .add_plugins((LogDiagnosticsPlugin::default(), FrameTimeDiagnosticsPlugin))
+        .run();
+}
+
+#[derive(Clone, Eq, PartialEq, Debug, Hash, Default, States)]
+pub enum AppState {
+    #[default]
+    Boot,
+    Menu,
+    Game,
+    Pause,
 }
