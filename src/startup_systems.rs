84970ae1a02cf467b3624ff98094f350bf826686
ilotterytea <ilotterytea@proton.me>
2023-11-29 00:14:08 +0600

    feat: spawn 3d camera + camera type enum

--- src/startup_systems.rs
+++ src/startup_systems.rs
@@ -1,5 +1,33 @@
 use bevy::prelude::*;
 
+#[derive(Component, PartialEq, Eq)]
+pub enum CameraType {
+    TwoD,
+    ThreeD,
+}
+
 pub fn spawn_2d_camera(mut commands: Commands) {
-    commands.spawn(Camera2dBundle::default());
+    commands.spawn((
+        Camera2dBundle {
+            camera: Camera {
+                order: 0,
+                ..default()
+            },
+            ..default()
+        },
+        CameraType::TwoD,
+    ));
+}
+
+pub fn spawn_3d_camera(mut commands: Commands) {
+    commands.spawn((
+        Camera3dBundle {
+            camera: Camera {
+                order: 1,
+                ..default()
+            },
+            ..default()
+        },
+        CameraType::ThreeD,
+    ));
 }
