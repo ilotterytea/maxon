763f4ae5a165de33eb0ca8a61282e9f28c5e6dc4
ilotterytea <ilotterytea@proton.me>
2024-01-21 13:20:34 +0600

    feat: camera position const

--- src/constants.rs
+++ src/constants.rs
@@ -6,3 +6,10 @@ pub const APP_DEVELOPER: &str = "ilotterytea";
 pub const APP_NAME: &str = "MaxonPettingSimulator";
 
 pub const ITEM_PRICE_MULTIPLIER: f64 = 1.15;
+
+pub const CAMERA_POSITIONS: [(
+    (f32, f32, f32),      // position (x,y,z)
+    (f32, f32, f32, f32), // rotation (x,y,z,angle)
+); 1] = [
+    ((4.0, 2.5, 4.0), (0.0, 1.0, 0.0, 0.784)), // Living room
+];
