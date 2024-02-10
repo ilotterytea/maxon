281c3481f7f33aa4ebb2ee162a85614492aa6428
ilotterytea <ilotterytea@proton.me>
2024-02-10 16:46:37 +0600

    feat: using gradient in movement buttons

--- src/game/basement/ui.rs
+++ src/game/basement/ui.rs
@@ -1,40 +1,79 @@
 use bevy::prelude::*;
 
+use crate::assets::AppAssets;
+
 #[derive(Component, PartialEq, Eq, PartialOrd, Ord)]
 pub(super) enum BuildingMovementButton {
     Left,
     Right,
 }
 
-pub(super) fn building_movement_buttons(mut commands: Commands) {
-    commands.spawn((
-        ButtonBundle {
-            style: Style {
-                position_type: PositionType::Absolute,
-                bottom: Val::Percent(15.0),
-                left: Val::Percent(0.0),
-                height: Val::Percent(100.0),
-                width: Val::Percent(15.0),
+pub(super) fn building_movement_buttons(mut commands: Commands, app_assets: Res<AppAssets>) {
+    commands
+        .spawn((
+            ButtonBundle {
+                style: Style {
+                    position_type: PositionType::Absolute,
+                    bottom: Val::Percent(0.0),
+                    left: Val::Percent(0.0),
+                    height: Val::Percent(100.0),
+                    width: Val::Percent(15.0),
+
+                    ..default()
+                },
+                background_color: Color::NONE.into(),
                 ..default()
             },
-            ..default()
-        },
-        BuildingMovementButton::Left,
-    ));
+            BuildingMovementButton::Left,
+        ))
+        .with_children(|parent| {
+            parent.spawn(AtlasImageBundle {
+                style: Style {
+                    width: Val::Percent(100.0),
+                    height: Val::Percent(100.0),
+                    ..default()
+                },
+                texture_atlas: app_assets.ui_gradient.clone(),
+                texture_atlas_image: UiTextureAtlasImage {
+                    index: 3,
+                    ..default()
+                },
+                background_color: Color::BLACK.into(),
+                ..default()
+            });
+        });
 
-    commands.spawn((
-        ButtonBundle {
-            style: Style {
-                position_type: PositionType::Absolute,
-                bottom: Val::Percent(15.0),
-                right: Val::Percent(0.0),
-                height: Val::Percent(100.0),
-                width: Val::Percent(15.0),
+    commands
+        .spawn((
+            ButtonBundle {
+                style: Style {
+                    position_type: PositionType::Absolute,
+                    bottom: Val::Percent(0.0),
+                    right: Val::Percent(0.0),
+                    height: Val::Percent(100.0),
+                    width: Val::Percent(15.0),
 
+                    ..default()
+                },
+                background_color: Color::NONE.into(),
                 ..default()
             },
-            ..default()
-        },
-        BuildingMovementButton::Right,
-    ));
+            BuildingMovementButton::Right,
+        ))
+        .with_children(|parent| {
+            parent.spawn(AtlasImageBundle {
+                style: Style {
+                    width: Val::Percent(100.0),
+                    height: Val::Percent(100.0),
+                    ..default()
+                },
+                texture_atlas: app_assets.ui_gradient.clone(),
+                texture_atlas_image: UiTextureAtlasImage {
+                    index: 0,
+                    ..default()
+                },
+                background_color: Color::BLACK.into(),
+                ..default()
+            });
+        });
 }
