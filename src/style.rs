f8f0dccfb7744d4646ae9ecc2c90627bfd4ed286
ilotterytea <ilotterytea@proton.me>
2024-02-11 01:53:39 +0600

    feat: building header text style

--- src/style.rs
+++ src/style.rs
@@ -35,3 +35,13 @@ pub fn get_category_header_text_style(font: Handle<Font>) -> TextStyle {
         color: ITEM_HEADER_ACTIVE_COLOR,
     }
 }
+
+pub const BUILDING_HEADER_COLOR: Color = Color::rgb(255.0 / 255.0, 245.0 / 255.0, 160.0 / 255.0);
+
+pub fn get_building_header_text_style(font: Handle<Font>) -> TextStyle {
+    TextStyle {
+        font,
+        font_size: 32.0,
+        color: BUILDING_HEADER_COLOR,
+    }
+}
