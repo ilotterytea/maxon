8f2abae39e2cf361c221237aef38af54a22fede2
ilotterytea <ilotterytea@proton.me>
2024-03-30 22:15:44 +0500

    feat: new text style + removed unused text styles

--- src/style.rs
+++ src/style.rs
@@ -2,39 +2,7 @@ use bevy::prelude::*;
 
 pub const CONTROLPANEL_BG_COLOR: Color = Color::rgba(0.0, 0.0, 0.0, 0.5);
 
-pub const ITEM_BG_INACTIVE_COLOR: Color = Color::rgb(0.25, 0.25, 0.25);
-pub const ITEM_BG_ACTIVE_COLOR: Color = Color::rgb(0.35, 0.35, 0.35);
-pub const ITEM_BORDER_COLOR: Color = Color::rgb(0.25, 0.25, 0.25);
-
-pub const ITEM_HEADER_INACTIVE_COLOR: Color = Color::rgb(0.4, 0.4, 0.4);
-pub const ITEM_HEADER_ACTIVE_COLOR: Color = Color::YELLOW;
-
-pub const ITEM_DESC_INACTIVE_COLOR: Color = Color::rgb(0.4, 0.4, 0.4);
-pub const ITEM_DESC_ACTIVE_COLOR: Color = Color::rgb(1.0, 1.0, 1.0);
-
-pub fn get_item_header_text_style(font: Handle<Font>) -> TextStyle {
-    TextStyle {
-        font,
-        font_size: 16.0,
-        color: ITEM_HEADER_ACTIVE_COLOR,
-    }
-}
-
-pub fn get_item_desc_text_style(font: Handle<Font>) -> TextStyle {
-    TextStyle {
-        font,
-        font_size: 14.0,
-        color: ITEM_DESC_ACTIVE_COLOR,
-    }
-}
-
-pub fn get_category_header_text_style(font: Handle<Font>) -> TextStyle {
-    TextStyle {
-        font,
-        font_size: 32.0,
-        color: ITEM_HEADER_ACTIVE_COLOR,
-    }
-}
+pub const MAIN_COLOR: Color = Color::rgb(1.0, 169.0 / 255.0, 44.0 / 255.0);
 
 pub const BUILDING_HEADER_COLOR: Color = Color::rgb(255.0 / 255.0, 245.0 / 255.0, 160.0 / 255.0);
 
@@ -43,5 +11,13 @@ pub fn get_building_header_text_style(font: Handle<Font>) -> TextStyle {
         font,
         font_size: 32.0,
         color: BUILDING_HEADER_COLOR,
+    }
+}
+
+pub fn get_savegame_ui_text_style(font: Handle<Font>) -> TextStyle {
+    TextStyle {
+        font,
+        font_size: 48.0,
+        color: MAIN_COLOR,
     }
 }
