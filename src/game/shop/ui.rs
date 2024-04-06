9e9977451042b39a78ac761d1f0987f311d7b5b6
ilotterytea <ilotterytea@proton.me>
2024-04-07 04:08:44 +0500

    feat: shop settings buttons

--- src/game/shop/ui.rs
+++ src/game/shop/ui.rs
@@ -11,6 +11,8 @@ use crate::{
     style::{DARKER_MAIN_COLOR, DARK_MAIN_COLOR, MAIN_COLOR},
 };
 
+use super::{ShopMode, ShopMultiplier};
+
 pub fn generate_shop_ui(
     mut commands: Commands,
     savegame: Res<Persistent<PlayerData>>,
@@ -159,6 +161,133 @@ pub fn generate_shop_ui(
                     ),
                     ..default()
                 });
+            });
+
+            // Shop settings buttons
+            root.spawn(NodeBundle {
+                style: Style {
+                    width: Val::Percent(100.0),
+                    display: Display::Flex,
+                    flex_direction: FlexDirection::Row,
+                    align_items: AlignItems::Center,
+                    justify_content: JustifyContent::Center,
+                    padding: UiRect::all(Val::Percent(1.0)),
+                    ..default()
+                },
+                background_color: MAIN_COLOR.into(),
+                ..default()
+            })
+            .with_children(|panel| {
+                let text_style = TextStyle {
+                    font: app_assets.font_text.clone(),
+                    font_size: 14.0,
+                    color: Color::WHITE,
+                };
+
+                let btn_style = Style {
+                    width: Val::Percent(100.0),
+                    flex_grow: 1.0,
+                    padding: UiRect::all(Val::Percent(2.0)),
+                    display: Display::Flex,
+                    align_items: AlignItems::Center,
+                    justify_content: JustifyContent::Center,
+                    ..default()
+                };
+
+                // Shop mode buttons
+                panel
+                    .spawn(NodeBundle {
+                        style: Style {
+                            height: Val::Percent(100.0),
+                            display: Display::Flex,
+                            flex_grow: 1.0,
+                            justify_content: JustifyContent::Center,
+                            align_items: AlignItems::Center,
+                            flex_direction: FlexDirection::Column,
+                            ..default()
+                        },
+                        ..default()
+                    })
+                    .with_children(|panel| {
+                        panel
+                            .spawn((
+                                ButtonBundle {
+                                    style: btn_style.clone(),
+                                    background_color: Color::NONE.into(),
+                                    ..default()
+                                },
+                                ShopMode::Buy,
+                            ))
+                            .with_children(|btn| {
+                                btn.spawn(TextBundle::from_section("Buy", text_style.clone()));
+                            });
+
+                        panel
+                            .spawn((
+                                ButtonBundle {
+                                    style: btn_style.clone(),
+                                    background_color: Color::NONE.into(),
+                                    ..default()
+                                },
+                                ShopMode::Sell,
+                            ))
+                            .with_children(|btn| {
+                                btn.spawn(TextBundle::from_section("Sell", text_style.clone()));
+                            });
+                    });
+
+                let sell_btn_style = Style {
+                    height: Val::Percent(100.0),
+                    aspect_ratio: Some(1.0),
+                    margin: UiRect::horizontal(Val::Percent(1.0)),
+                    padding: UiRect::all(Val::Percent(5.0)),
+                    display: Display::Flex,
+                    justify_content: JustifyContent::Center,
+                    align_items: AlignItems::Center,
+                    ..default()
+                };
+
+                // Shop multiplier buttons
+                panel
+                    .spawn(NodeBundle {
+                        style: Style {
+                            height: Val::Percent(100.0),
+                            display: Display::Flex,
+                            flex_grow: 2.0,
+                            align_items: AlignItems::Center,
+                            flex_direction: FlexDirection::Row,
+                            ..default()
+                        },
+                        background_color: Color::NONE.into(),
+                        ..default()
+                    })
+                    .with_children(|panel| {
+                        panel
+                            .spawn((
+                                ButtonBundle {
+                                    style: sell_btn_style.clone(),
+                                    background_color: Color::DARK_GRAY.into(),
+                                    ..default()
+                                },
+                                ShopMultiplier::X1,
+                            ))
+                            .with_children(|btn| {
+                                btn.spawn(TextBundle::from_section("1x", text_style.clone()));
+                            });
+
+                        panel
+                            .spawn((
+                                ButtonBundle {
+                                    style: sell_btn_style.clone(),
+                                    background_color: Color::DARK_GRAY.into(),
+                                    ..default()
+                                },
+                                ShopMultiplier::X10,
+                            ))
+                            .with_children(|btn| {
+                                btn.spawn(TextBundle::from_section("10x", text_style.clone()));
+                            });
+                    });
             });
 
             let mut shop_item_list = root.spawn(NodeBundle {
