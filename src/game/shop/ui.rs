98c5e47fac8490ff5f4563c0dbae0eefa938f1bc
ilotterytea <ilotterytea@proton.me>
2024-04-09 00:34:51 +0500

    feat: added name for every component in shop ui

--- src/game/shop/ui.rs
+++ src/game/shop/ui.rs
@@ -59,6 +59,7 @@ pub fn generate_shop_ui(
                 },
                 b.building.clone(),
                 UiUnitComponent,
+                Name::new(format!("Shop slot: {}", b.building.to_string())),
             ))
             .with_children(|parent| {
                 // Info
@@ -74,6 +75,7 @@ pub fn generate_shop_ui(
                             ..default()
                         },
                         UiUnitItemInfoComponent,
+                        Name::new(format!("Shop slot info: {}", b.building.to_string())),
                     ))
                     .with_children(|info| {
                         let icon_style = Style {
@@ -85,11 +87,17 @@ pub fn generate_shop_ui(
                         // Item icon
                         match b.building.get_image_handles(&app_assets).1 {
                             BuildingCharacter::Static(v) => {
-                                info.spawn(ImageBundle {
-                                    style: icon_style,
-                                    image: UiImage::new(v),
-                                    ..default()
-                                });
+                                info.spawn((
+                                    ImageBundle {
+                                        style: icon_style,
+                                        image: UiImage::new(v),
+                                        ..default()
+                                    },
+                                    Name::new(format!(
+                                        "Shop slot icon: {}",
+                                        b.building.to_string()
+                                    )),
+                                ));
                             }
                             BuildingCharacter::Animated(v, a) => {
                                 info.spawn((
@@ -99,6 +107,10 @@ pub fn generate_shop_ui(
                                         ..default()
                                     },
                                     a,
+                                    Name::new(format!(
+                                        "Shop slot icon: {}",
+                                        b.building.to_string()
+                                    )),
                                 ));
                             }
                         }
@@ -115,16 +127,20 @@ pub fn generate_shop_ui(
                                 ..default()
                             },
                             UiUnitItemDescComponent,
+                            Name::new(format!("Shop slot desc: {}", b.building.to_string())),
                         ))
                         .with_children(|desc| {
                             // Item name
-                            desc.spawn(TextBundle::from_section(
-                                b.building.to_string(),
-                                TextStyle {
-                                    font: app_assets.font_text.clone(),
-                                    font_size: 18.0,
-                                    color: Color::BLACK.into(),
-                                },
+                            desc.spawn((
+                                TextBundle::from_section(
+                                    b.building.to_string(),
+                                    TextStyle {
+                                        font: app_assets.font_text.clone(),
+                                        font_size: 18.0,
+                                        color: Color::BLACK.into(),
+                                    },
+                                ),
+                                Name::new(format!("Shop slot name: {}", b.building.to_string())),
                             ));
 
                             // Item price
@@ -137,6 +153,7 @@ pub fn generate_shop_ui(
                                         color: Color::BEIGE.into(),
                                     },
                                 ),
+                                Name::new(format!("Shop slot price: {}", b.building.to_string())),
                                 UiUnitPriceTextComponent,
                             ));
                         });
@@ -165,46 +182,55 @@ pub fn generate_shop_ui(
             Name::new("Shop UI"),
         ))
         .with_children(|root| {
-            root.spawn(NodeBundle {
-                style: Style {
-                    width: Val::Percent(100.0),
-                    padding: UiRect::all(Val::Percent(5.0)),
-                    display: Display::Flex,
-                    justify_content: JustifyContent::Center,
-                    align_items: AlignItems::Center,
+            root.spawn((
+                NodeBundle {
+                    style: Style {
+                        width: Val::Percent(100.0),
+                        padding: UiRect::all(Val::Percent(5.0)),
+                        display: Display::Flex,
+                        justify_content: JustifyContent::Center,
+                        align_items: AlignItems::Center,
+                        ..default()
+                    },
+                    background_color: MAIN_COLOR.into(),
                     ..default()
                 },
-                background_color: MAIN_COLOR.into(),
-                ..default()
-            })
+                Name::new("Shop title"),
+            ))
             .with_children(|panel| {
-                panel.spawn(TextBundle {
-                    text: Text::from_section(
-                        "Shop",
-                        TextStyle {
-                            font: app_assets.font_text.clone(),
-                            font_size: 48.0,
-                            color: Color::BLACK.into(),
-                        },
-                    ),
-                    ..default()
-                });
+                panel.spawn((
+                    TextBundle {
+                        text: Text::from_section(
+                            "Shop",
+                            TextStyle {
+                                font: app_assets.font_text.clone(),
+                                font_size: 48.0,
+                                color: Color::BLACK.into(),
+                            },
+                        ),
+                        ..default()
+                    },
+                    Name::new("Shop title text"),
+                ));
             });
 
             // Shop settings buttons
-            root.spawn(NodeBundle {
-                style: Style {
-                    width: Val::Percent(100.0),
-                    display: Display::Flex,
-                    flex_direction: FlexDirection::Row,
-                    align_items: AlignItems::Center,
-                    justify_content: JustifyContent::Center,
-                    padding: UiRect::all(Val::Percent(1.0)),
+            root.spawn((
+                NodeBundle {
+                    style: Style {
+                        width: Val::Percent(100.0),
+                        display: Display::Flex,
+                        flex_direction: FlexDirection::Row,
+                        align_items: AlignItems::Center,
+                        justify_content: JustifyContent::Center,
+                        padding: UiRect::all(Val::Percent(1.0)),
+                        ..default()
+                    },
+                    background_color: MAIN_COLOR.into(),
                     ..default()
                 },
-                background_color: MAIN_COLOR.into(),
-                ..default()
-            })
+                Name::new("Shop settings"),
+            ))
             .with_children(|panel| {
                 let text_style = TextStyle {
                     font: app_assets.font_text.clone(),
@@ -224,18 +250,21 @@ pub fn generate_shop_ui(
 
                 // Shop mode buttons
                 panel
-                    .spawn(NodeBundle {
-                        style: Style {
-                            height: Val::Percent(100.0),
-                            display: Display::Flex,
-                            flex_grow: 1.0,
-                            justify_content: JustifyContent::Center,
-                            align_items: AlignItems::Center,
-                            flex_direction: FlexDirection::Column,
+                    .spawn((
+                        NodeBundle {
+                            style: Style {
+                                height: Val::Percent(100.0),
+                                display: Display::Flex,
+                                flex_grow: 1.0,
+                                justify_content: JustifyContent::Center,
+                                align_items: AlignItems::Center,
+                                flex_direction: FlexDirection::Column,
+                                ..default()
+                            },
                             ..default()
                         },
-                        ..default()
-                    })
+                        Name::new("Shop mode"),
+                    ))
                     .with_children(|panel| {
                         panel
                             .spawn((
@@ -245,9 +274,13 @@ pub fn generate_shop_ui(
                                     ..default()
                                 },
                                 ShopMode::Buy,
+                                Name::new("Shop mode: buy"),
                             ))
                             .with_children(|btn| {
-                                btn.spawn(TextBundle::from_section("Buy", text_style.clone()));
+                                btn.spawn((
+                                    TextBundle::from_section("Buy", text_style.clone()),
+                                    Name::new("Shop mode text: buy"),
+                                ));
                             });
 
                         panel
@@ -258,9 +291,13 @@ pub fn generate_shop_ui(
                                     ..default()
                                 },
                                 ShopMode::Sell,
+                                Name::new("Shop mode: sell"),
                             ))
                             .with_children(|btn| {
-                                btn.spawn(TextBundle::from_section("Sell", text_style.clone()));
+                                btn.spawn((
+                                    TextBundle::from_section("Sell", text_style.clone()),
+                                    Name::new("Shop mode text: sell"),
+                                ));
                             });
                     });
 
@@ -277,18 +314,21 @@ pub fn generate_shop_ui(
 
                 // Shop multiplier buttons
                 panel
-                    .spawn(NodeBundle {
-                        style: Style {
-                            height: Val::Percent(100.0),
-                            display: Display::Flex,
-                            flex_grow: 2.0,
-                            align_items: AlignItems::Center,
-                            flex_direction: FlexDirection::Row,
+                    .spawn((
+                        NodeBundle {
+                            style: Style {
+                                height: Val::Percent(100.0),
+                                display: Display::Flex,
+                                flex_grow: 2.0,
+                                align_items: AlignItems::Center,
+                                flex_direction: FlexDirection::Row,
+                                ..default()
+                            },
+                            background_color: Color::NONE.into(),
                             ..default()
                         },
-                        background_color: Color::NONE.into(),
-                        ..default()
-                    })
+                        Name::new("Shop multiplier"),
+                    ))
                     .with_children(|panel| {
                         panel
                             .spawn((
@@ -298,9 +338,13 @@ pub fn generate_shop_ui(
                                     ..default()
                                 },
                                 ShopMultiplier::X1,
+                                Name::new("Shop multiplier: 1x"),
                             ))
                             .with_children(|btn| {
-                                btn.spawn(TextBundle::from_section("1x", text_style.clone()));
+                                btn.spawn((
+                                    TextBundle::from_section("1x", text_style.clone()),
+                                    Name::new("Shop multiplier text: 1x"),
+                                ));
                             });
 
                         panel
@@ -311,24 +355,31 @@ pub fn generate_shop_ui(
                                     ..default()
                                 },
                                 ShopMultiplier::X10,
+                                Name::new("Shop multiplier: 10x"),
                             ))
                             .with_children(|btn| {
-                                btn.spawn(TextBundle::from_section("10x", text_style.clone()));
+                                btn.spawn((
+                                    TextBundle::from_section("10x", text_style.clone()),
+                                    Name::new("Shop multiplier text: 10x"),
+                                ));
                             });
                     });
             });
 
-            let mut shop_item_list = root.spawn(NodeBundle {
-                style: Style {
-                    width: Val::Percent(100.0),
-                    flex_grow: 3.0,
-                    display: Display::Flex,
-                    flex_direction: FlexDirection::Column,
+            let mut shop_item_list = root.spawn((
+                NodeBundle {
+                    style: Style {
+                        width: Val::Percent(100.0),
+                        flex_grow: 3.0,
+                        display: Display::Flex,
+                        flex_direction: FlexDirection::Column,
+                        ..default()
+                    },
+                    background_color: DARKER_MAIN_COLOR.into(),
                     ..default()
                 },
-                background_color: DARKER_MAIN_COLOR.into(),
-                ..default()
-            });
+                Name::new("Shop item list"),
+            ));
 
             for e in shop_item_ids {
                 shop_item_list.add_child(e);
