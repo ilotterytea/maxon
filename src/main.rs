3c46e4682f92e9e82359664714c908afb48cd9a3
ilotterytea <ilotterytea@proton.me>
2024-04-11 15:25:01 +0500

    feat: debug feature

--- src/main.rs
+++ src/main.rs
@@ -29,42 +29,49 @@ mod startup_systems;
 mod style;
 
 fn main() {
-    App::new()
-        .add_plugins((
-            DefaultPlugins.set(WindowPlugin {
-                primary_window: Some(Window {
-                    title: "Maxon Petting Simulator".into(),
-                    present_mode: PresentMode::AutoVsync,
-                    ..default()
-                }),
+    let mut app = App::new();
+
+    app.add_plugins((
+        DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "Maxon Petting Simulator".into(),
+                present_mode: PresentMode::AutoVsync,
                 ..default()
             }),
-            JsonAssetPlugin::<Localization>::new(&["locale.json"]),
-        ))
-        // App states
-        .add_state::<AppState>()
-        // Initializing startup systems
-        .add_systems(Startup, (spawn_3d_camera, init_settings))
-        // Loading state
-        .add_loading_state(LoadingState::new(AppState::Boot).continue_to_state(AppState::Menu))
-        .add_collection_to_loading_state::<_, AppAssets>(AppState::Boot)
-        .add_plugins((GamePlugin, MenuPlugin))
-        // 3D Sprites
-        .add_plugins(Sprite3dPlugin)
-        // Diagnostics
-        .add_plugins((LogDiagnosticsPlugin::default(), FrameTimeDiagnosticsPlugin))
-        // World inspector
-        .add_plugins(WorldInspectorPlugin::new())
-        // 3D Picking
-        .add_plugins(DefaultPickingPlugins)
-        .insert_resource(State::new(DebugPickingMode::Disabled))
-        // RNG
-        .add_plugins(RngPlugin::default())
-        // Easings (animation)
-        .add_plugins(EasingsPlugin)
-        // Billboard
-        .add_plugins(BillboardPlugin)
-        .run();
+            ..default()
+        }),
+        JsonAssetPlugin::<Localization>::new(&["locale.json"]),
+    ))
+    // App states
+    .add_state::<AppState>()
+    // Initializing startup systems
+    .add_systems(Startup, (spawn_3d_camera, init_settings))
+    // Loading state
+    .add_loading_state(LoadingState::new(AppState::Boot).continue_to_state(AppState::Menu))
+    .add_collection_to_loading_state::<_, AppAssets>(AppState::Boot)
+    .add_plugins((GamePlugin, MenuPlugin))
+    // 3D Sprites
+    .add_plugins(Sprite3dPlugin)
+    // 3D Picking
+    .add_plugins(DefaultPickingPlugins)
+    .insert_resource(State::new(DebugPickingMode::Disabled))
+    // RNG
+    .add_plugins(RngPlugin::default())
+    // Easings (animation)
+    .add_plugins(EasingsPlugin)
+    // Billboard
+    .add_plugins(BillboardPlugin);
+
+    #[cfg(feature = "debug")]
+    {
+        app
+            // Diagnostics
+            .add_plugins((LogDiagnosticsPlugin::default(), FrameTimeDiagnosticsPlugin))
+            // World inspector
+            .add_plugins(WorldInspectorPlugin::new());
+    }
+
+    app.run();
 }
 
 #[derive(Clone, Eq, PartialEq, Debug, Hash, Default, States)]
