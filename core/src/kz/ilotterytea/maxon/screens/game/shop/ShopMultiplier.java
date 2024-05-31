98d428b55e2f7745d8ea8b5e65c24ff245bbc3d5
ilotterytea <me@ilotterytea.kz>
2024-06-01 00:51:20 +0500

    upd: moved the whole project under kz.ilotterytea.maxon name

---
+++ core/src/kz/ilotterytea/maxon/screens/game/shop/ShopMultiplier.java
@@ -0,0 +1,17 @@
+package kz.ilotterytea.maxon.screens.game.shop;
+
+public enum ShopMultiplier {
+    X1(1),
+    X10(10),
+    ;
+
+    private final int multiplier;
+
+    ShopMultiplier(int multiplier) {
+        this.multiplier = multiplier;
+    }
+
+    public int getMultiplier() {
+        return multiplier;
+    }
+}
