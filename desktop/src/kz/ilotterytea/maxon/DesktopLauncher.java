dc99bd738b1a4b38112f767c3253724d0ff0587c
ilotterytea <iltsu@alright.party>
2024-06-07 20:27:38 +0500

    del: splashes

--- desktop/src/kz/ilotterytea/maxon/DesktopLauncher.java
+++ desktop/src/kz/ilotterytea/maxon/DesktopLauncher.java
@@ -3,39 +3,14 @@ package kz.ilotterytea.maxon;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 
-import java.util.ArrayList;
-import java.util.Objects;
-import java.util.Scanner;
-
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
-		config.setTitle(String.format("%s %s: %s", MaxonConstants.GAME_NAME, MaxonConstants.GAME_VERSION, getRandomLine()));
+		config.setTitle(MaxonConstants.GAME_NAME);
 		config.setWindowIcon("icon_chest.png");
 
 		new Lwjgl3Application(MaxonGame.getInstance(), config);
-	}
-
-	private static String getRandomLine() {
-		String line = "missingno";
-
-		try {
-			Scanner scanner = new Scanner(
-					Objects.requireNonNull(Thread.currentThread().getContextClassLoader().getResourceAsStream("texts/splashes.txt"))
-			);
-			ArrayList<String> strings = new ArrayList<>();
-
-			while (scanner.hasNext()) {
-				strings.add(scanner.next());
-			}
-
-			line = strings.get((int) Math.floor(Math.random() * strings.size()));
-		} catch (NullPointerException e) {
-			e.printStackTrace();
-		}
-
-		return line;
 	}
 }
