3394e5ff99ed05d16c9b1a09d93d76d047c839f1
ilotterytea <me@ilotterytea.kz>
2024-07-13 14:39:44 +0500

    feat: bevy engine + debug feature

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let mut app = App::new();
+
+    app.add_plugins(DefaultPlugins.set(WindowPlugin {
+        primary_window: Some(Window {
+            title: "Maxon Petting Simulator".into(),
+            resolution: (800., 600.).into(),
+            ..default()
+        }),
+        ..default()
+    }));
+
+    #[cfg(feature = "debug")]
+    {
+        // Diagnostics
+        app.add_plugins((
+            bevy::diagnostic::FrameTimeDiagnosticsPlugin,
+            bevy::diagnostic::LogDiagnosticsPlugin::default(),
+        ))
+    }
+
+    app.run();
 }
