6209d211a1be2d0e48504a16a8313091828cbdd1
ilotterytea <me@ilotterytea.kz>
2024-07-13 17:55:23 +0500

    fix: there's actually 10 columns

--- src/assets.rs
+++ src/assets.rs
@@ -9,7 +9,7 @@ pub struct ModelAssets {
 
 #[derive(AssetCollection, Resource)]
 pub struct TextureAtlasAssets {
-    #[asset(texture_atlas_layout(tile_size_x = 112, tile_size_y = 112, columns = 11, rows = 5))]
+    #[asset(texture_atlas_layout(tile_size_x = 112, tile_size_y = 112, columns = 10, rows = 5))]
     pub player_layout: Handle<TextureAtlasLayout>,
 
     #[asset(path = "sprites/sheet/loadingCircle.png")]
