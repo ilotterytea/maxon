041a6b9641fdee85eadd077cffa4faff2cd0445a
ilotterytea <me@ilotterytea.kz>
2024-07-30 13:19:42 +0500

    feat: apply settings at startup

--- src/boot.rs
+++ src/boot.rs
@@ -1,16 +1,34 @@
-use bevy::prelude::*;
+use bevy::{
+    prelude::*,
+    window::{PrimaryWindow, WindowMode},
+};
+use bevy_persistent::Persistent;
 
-use crate::{localization::setup_localization, AppState};
+use crate::{localization::setup_localization, persistent::Settings, AppState};
 
 pub struct BootPlugin;
 
 impl Plugin for BootPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(OnEnter(AppState::Boot), setup_localization)
-            .add_systems(Update, move_to_menu_screen.run_if(in_state(AppState::Boot)));
+        app.add_systems(
+            OnEnter(AppState::Boot),
+            (setup_localization, apply_settings),
+        )
+        .add_systems(Update, move_to_menu_screen.run_if(in_state(AppState::Boot)));
     }
 }
 
 fn move_to_menu_screen(mut state: ResMut<NextState<AppState>>) {
     state.set(AppState::Menu);
+}
+
+fn apply_settings(
+    settings: Res<Persistent<Settings>>,
+    mut window: Query<&mut Window, With<PrimaryWindow>>,
+) {
+    let mut window = window.single_mut();
+
+    if settings.is_fullscreen {
+        window.mode = WindowMode::BorderlessFullscreen;
+    }
 }
