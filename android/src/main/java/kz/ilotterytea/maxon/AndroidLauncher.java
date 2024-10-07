c5ee68a647871ca7bc8044a96759e7c00f9188db
ilotterytea <iltsu@alright.party>
2024-10-08 01:54:47 +0500

    feat: Android support (again)

---
+++ android/src/main/java/kz/ilotterytea/maxon/AndroidLauncher.java
@@ -0,0 +1,15 @@
+package kz.ilotterytea.maxon;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+
+public class AndroidLauncher extends AndroidApplication {
+    @Override
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+        initialize(MaxonGame.getInstance(), config);
+    }
+}
