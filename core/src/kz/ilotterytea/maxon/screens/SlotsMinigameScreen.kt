fe796ce5b0cc568cb7d8a113306f363ebd524ff2
ilotterytea <me@ilotterytea.kz>
2024-10-25 22:57:14 +0500

    upd: use hide() method to save game

--- core/src/kz/ilotterytea/maxon/screens/SlotsMinigameScreen.kt
+++ core/src/kz/ilotterytea/maxon/screens/SlotsMinigameScreen.kt
@@ -1,9 +1,6 @@
 package kz.ilotterytea.maxon.screens
 
 import com.badlogic.gdx.Gdx
-import com.badlogic.gdx.Input
-import com.badlogic.gdx.InputMultiplexer
-import com.badlogic.gdx.InputProcessor
 import com.badlogic.gdx.Screen
 import com.badlogic.gdx.assets.AssetManager
 import com.badlogic.gdx.audio.Music
@@ -14,20 +11,13 @@ import com.badlogic.gdx.graphics.g2d.TextureAtlas
 import com.badlogic.gdx.scenes.scene2d.Actor
 import com.badlogic.gdx.scenes.scene2d.InputEvent
 import com.badlogic.gdx.scenes.scene2d.Stage
-import com.badlogic.gdx.scenes.scene2d.ui.Image
-import com.badlogic.gdx.scenes.scene2d.ui.Label
-import com.badlogic.gdx.scenes.scene2d.ui.Skin
-import com.badlogic.gdx.scenes.scene2d.ui.Stack
-import com.badlogic.gdx.scenes.scene2d.ui.Table
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton
-import com.badlogic.gdx.scenes.scene2d.ui.TextField
+import com.badlogic.gdx.scenes.scene2d.ui.*
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener
 import com.badlogic.gdx.utils.Align
 import com.badlogic.gdx.utils.Timer
 import com.badlogic.gdx.utils.Timer.Task
 import com.badlogic.gdx.utils.viewport.FitViewport
-import com.badlogic.gdx.utils.viewport.ScreenViewport
 import kz.ilotterytea.maxon.MaxonGame
 import kz.ilotterytea.maxon.localization.LineId
 import kz.ilotterytea.maxon.player.Savegame
@@ -396,6 +386,7 @@ class SlotsMinigameScreen : Screen {
     }
 
     override fun hide() {
+        savegame.save()
         dispose()
     }
 
