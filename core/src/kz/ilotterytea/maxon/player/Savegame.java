b06984f81f8a432d441fb13e31f888786211cdfc
ilotterytea <iltsu@alright.party>
2025-10-30 21:23:16 +0500

    upd: use JsonWriter and JsonReader for savegame serialization

--- core/src/kz/ilotterytea/maxon/player/Savegame.java
+++ core/src/kz/ilotterytea/maxon/player/Savegame.java
@@ -4,22 +4,21 @@ package kz.ilotterytea.maxon.player;
 import com.badlogic.gdx.Application;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.files.FileHandle;
-import com.badlogic.gdx.utils.GdxRuntimeException;
-import com.badlogic.gdx.utils.Json;
-import com.badlogic.gdx.utils.Logger;
-import com.badlogic.gdx.utils.SerializationException;
+import com.badlogic.gdx.utils.*;
 import kz.ilotterytea.maxon.MaxonConstants;
 import kz.ilotterytea.maxon.utils.OsUtils;
 
-import java.io.Serializable;
+import java.io.IOException;
+import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class Savegame implements Serializable {
-    private static final FileHandle directory = Gdx.files.absolute(MaxonConstants.GAME_SAVEGAME_FOLDER);
-    private static FileHandle file;
+public class Savegame {
+    private static FileHandle directory, file;
 
     private static final Logger logger = new Logger(Savegame.class.getName());
+
+    private static Savegame data;
 
     private double money, multiplier;
     private final HashMap<String, Integer> purchasedPets = new HashMap<>();
@@ -28,14 +27,12 @@ public class Savegame implements Serializable {
     private long elapsedTime, slotsWins, slotsTotalSpins;
     private boolean isNewlyCreated;
 
-    private static Savegame savegame;
-
     public static Savegame getInstance() {
-        if (savegame == null) {
-            savegame = load();
+        if (data == null) {
+            data = load();
         }
 
-        return savegame;
+        return data;
     }
 
     private Savegame() {
@@ -46,6 +43,7 @@ public class Savegame implements Serializable {
         if (Gdx.app.getType() == Application.ApplicationType.Android) {
             file = Gdx.files.absolute(Gdx.files.getExternalStoragePath() + "/savegame.maxon");
         } else {
+            directory = Gdx.files.absolute(MaxonConstants.GAME_SAVEGAME_FOLDER);
             file = Gdx.files.absolute(directory.path() + "/savegame.maxon");
         }
 
@@ -53,21 +51,31 @@ public class Savegame implements Serializable {
             return new Savegame();
         }
 
-        Savegame savegame = null;
+        JsonReader reader = new JsonReader();
+        JsonValue root = reader.parse(file);
 
-        try {
-            savegame = new Json().fromJson(Savegame.class, file);
-        } catch (SerializationException e) {
-            logger.error("Failed to parse the savegame", e);
-            file.delete();
+        Savegame savegame = new Savegame();
+        savegame.money = root.getDouble("money", 0.0f);
+        savegame.multiplier = root.getDouble("multiplier", 0.0f);
+        savegame.name = root.getString("name", OsUtils.isPC ? System.getProperty("user.name", "Maxon") : "Maxon");
+        savegame.elapsedTime = root.getLong("elapsedTime", 0);
+        savegame.slotsWins = root.getLong("slotsWins", 0);
+        savegame.slotsTotalSpins = root.getLong("slotsTotalSpins", 0);
+        savegame.isNewlyCreated = root.getBoolean("isNewlyCreated", false);
+
+        JsonValue pets = root.get("purchasedPets");
+        if (pets != null) {
+            for (JsonValue e = pets.child; e != null; e = e.next) {
+                savegame.purchasedPets.put(e.name, e.asInt());
+            }
         }
 
-        if (savegame == null) {
-            logger.error("Failed to load a save");
-            return new Savegame();
+        JsonValue unlockedPets = root.get("unlockedPets");
+        if (unlockedPets != null) {
+            for (JsonValue e = unlockedPets.child; e != null; e = e.next) {
+                savegame.unlockedPets.add(e.asString());
+            }
         }
-
-        savegame.isNewlyCreated = false;
 
         logger.info("Loaded the savegame");
 
@@ -75,12 +83,46 @@ public class Savegame implements Serializable {
     }
 
     public void save() {
-        if (OsUtils.isPC && !directory.exists()) {
+        if (OsUtils.isPC && directory != null && !directory.exists()) {
             directory.mkdirs();
         }
 
+        StringWriter out = new StringWriter();
+        JsonWriter writer = new JsonWriter(out);
+
         try {
-            file.writeString(new Json().toJson(this), false);
+            writer.object();
+            writer.name("money").value(money);
+            writer.name("multiplier").value(multiplier);
+            writer.name("name").value(name);
+            writer.name("elapsedTime").value(elapsedTime);
+            writer.name("slotsWins").value(slotsWins);
+            writer.name("slotsTotalSpins").value(slotsTotalSpins);
+            writer.name("isNewlyCreated").value(isNewlyCreated);
+
+            writer.name("purchasedPets");
+            writer.object();
+            for (var e : purchasedPets.entrySet()) {
+                writer.name(e.getKey()).value(e.getValue());
+            }
+            writer.pop();
+
+            writer.name("unlockedPets");
+            writer.array();
+            for (String s : unlockedPets) {
+                writer.value(s);
+            }
+            writer.pop();
+
+            writer.pop();
+            writer.close();
+        } catch (IOException e) {
+            logger.error("Failed to serialize the savegame", e);
+            return;
+        }
+
+        try {
+            file.writeString(out.toString(), false);
             logger.info("Saved the game");
         } catch (GdxRuntimeException e) {
             logger.error("Failed to save the game", e);
